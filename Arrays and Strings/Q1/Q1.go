1215311664732bc768a088a2d1a30602a9557c8d
jurkog <jurko.guba@gmail.com>
2015-04-28 21:31:48 -0400

    Q1 Uploaded

---
+++ Arrays and Strings/Q1/Q1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"	
+)
+
+func main() {
+	reader:=bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a string to test its uniqueness")
+	test,_ := reader.ReadString('\n')
+	if unique(test) {
+		fmt.Println("The string is unique!")
+	} else {
+		fmt.Println("The string contains duplicates of characters.")
+	}
+}
+
+// Decided to make it with the use of an additonal data structure (the boolean array)
+
+func unique(s string) bool {
+	b := []byte(s)
+	d := make([]bool, 95)
+	for i := range b {		
+		b[i] -= 33
+		if b[i] >= 0 && b[i] <= 94 {
+			if d[b[i]] {
+				return false
+			} else {
+				d[b[i]] = true;
+			}
+		}					
+	}
+	return true
+}
